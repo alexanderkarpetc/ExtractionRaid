087885c9fb03dba6fe2e46e80af4ce0a695212c5
Oleksandr Karpets <o.karpets@shgames.gg>
2026-04-06 15:21:29 +0200

    Add layout for context menu

--- Assets/Scripts/View/UI/ContextMenuView.cs
+++ Assets/Scripts/View/UI/ContextMenuView.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace View.UI
@@ -8,29 +9,24 @@ namespace View.UI
     [RequireComponent(typeof(CanvasGroup))]
     public class ContextMenuView : MonoBehaviour
     {
-        [SerializeField] Button _buttonTemplate;
-        [SerializeField] RectTransform _container;
+        [SerializeField] Button _button;
+
+        [Header("Row Colors")]
+        [SerializeField] Color _normalLabelColor = new Color(1f, 1f, 1f, 0.75f);
+        [SerializeField] Color _hoverBackgroundColor = new Color(0.878f, 0.314f, 0.251f, 0.10f);
+        [SerializeField] Color _hoverLabelColor = new Color(0.878f, 0.314f, 0.251f, 1f);
 
         CanvasGroup _canvasGroup;
         Canvas _rootCanvas;
-
-        readonly struct Entry
-        {
-            public readonly Button Button;
-            public readonly TMP_Text Label;
-            public Entry(Button b, TMP_Text l) { Button = b; Label = l; }
-        }
-
-        Entry[] _entries;
-        int _activeCount;
+        TMP_Text _label;
         Action<int> _callback;
 
         void Awake()
         {
             _canvasGroup = GetComponent<CanvasGroup>();
             _rootCanvas = GetComponentInParent<Canvas>()?.rootCanvas;
-            if (_buttonTemplate != null)
-                _buttonTemplate.gameObject.SetActive(false);
+            _label = _button.GetComponentInChildren<TMP_Text>();
+            SetupButtonColors();
             HideImmediate();
         }
 
@@ -40,20 +36,12 @@ namespace View.UI
         public void Show(Vector2 position, string[] options, Action<int> onSelect)
         {
             _callback = onSelect;
-            EnsureEntries(options.Length);
 
-            for (int i = 0; i < options.Length; i++)
-            {
-                var e = _entries[i];
-                e.Label.text = options[i];
-                e.Button.gameObject.SetActive(true);
-                int idx = i;
-                e.Button.onClick.RemoveAllListeners();
-                e.Button.onClick.AddListener(() => OnOptionClicked(idx));
-            }
-            for (int i = options.Length; i < _activeCount; i++)
-                _entries[i].Button.gameObject.SetActive(false);
-            _activeCount = options.Length;
+            _label.text = options[0];
+            _label.color = _normalLabelColor;
+            _button.gameObject.SetActive(true);
+            _button.onClick.RemoveAllListeners();
+            _button.onClick.AddListener(() => OnOptionClicked(0));
 
             PositionAt(position);
             _canvasGroup.alpha = 1f;
@@ -89,24 +77,26 @@ namespace View.UI
             ((RectTransform)transform).localPosition = localPoint;
         }
 
-        void EnsureEntries(int count)
+        void SetupButtonColors()
         {
-            if (_entries != null && _entries.Length >= count) return;
+            var colors = _button.colors;
+            colors.highlightedColor = _hoverBackgroundColor;
+            colors.pressedColor = new Color(_hoverBackgroundColor.r, _hoverBackgroundColor.g, _hoverBackgroundColor.b,
+                Mathf.Clamp01(_hoverBackgroundColor.a * 2.5f));
+            colors.selectedColor = _hoverBackgroundColor;
+            colors.colorMultiplier = 1f;
+            _button.colors = colors;
 
-            int oldLen = _entries?.Length ?? 0;
-            var prev = _entries;
-            _entries = new Entry[count];
-            if (prev != null)
-                Array.Copy(prev, _entries, oldLen);
+            var trigger = _button.GetComponent<EventTrigger>() ?? _button.gameObject.AddComponent<EventTrigger>();
+            trigger.triggers.Clear();
 
-            for (int i = oldLen; i < count; i++)
-            {
-                var go = Instantiate(_buttonTemplate.gameObject, _container);
-                go.SetActive(false);
-                var btn = go.GetComponent<Button>();
-                var lbl = go.GetComponentInChildren<TMP_Text>();
-                _entries[i] = new Entry(btn, lbl);
-            }
+            var onEnter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+            onEnter.callback.AddListener(_ => _label.color = _hoverLabelColor);
+            trigger.triggers.Add(onEnter);
+
+            var onExit = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+            onExit.callback.AddListener(_ => _label.color = _normalLabelColor);
+            trigger.triggers.Add(onExit);
         }
 
         void HideImmediate()
