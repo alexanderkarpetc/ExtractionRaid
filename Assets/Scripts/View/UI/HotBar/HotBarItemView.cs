1c6e59a3ae30ff223b2f404d8bee21ceb702e702
Oleksandr Karpets <o.karpets@shgames.gg>
2026-04-08 18:06:52 +0200

    Add prefabs

--- Assets/Scripts/View/UI/HotBar/HotBarItemView.cs
+++ Assets/Scripts/View/UI/HotBar/HotBarItemView.cs
@@ -4,6 +4,7 @@ namespace View.UI.HotBar
 {
     public class HotBarItemView : MonoBehaviour
     {
-    
+        [SerializeField] InventorySlotView _slotView;
+        [SerializeField] GameObject _emptyRoot;
     }
 }
--- Assets/Scripts/View/UI/HotBar/HotBarView.cs
+++ Assets/Scripts/View/UI/HotBar/HotBarView.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace View.UI.HotBar
 {
     public class HotBarView : MonoBehaviour
     {
-    
+        [SerializeField] List<HotBarItemView> _itemViews;
     }
 }
